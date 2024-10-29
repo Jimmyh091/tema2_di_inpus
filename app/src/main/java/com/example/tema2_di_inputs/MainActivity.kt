f393a556cf8c6630b5f2f954776cee7384c83dde
Jimmyh091 <jaimecaballero164@gmail.com>
2024-10-29 16:37:08 +0100

    TODOOO VAMOOOSOOOOOOS EL EJERCICIO 1 JODEEEEEER

--- app/src/main/java/com/example/tema2_di_inputs/MainActivity.kt
+++ app/src/main/java/com/example/tema2_di_inputs/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.example.tema2_di_inputs
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        validar()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -18,25 +19,47 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var boton = findViewById<Button>(R.id.button)
+        boton.setOnClickListener {
+            validarNombre()
+            validarEdad()
+        }
     }
 
-    fun validar() : Boolean{
-        val textInputLayout = findViewById<TextInputLayout>(R.id.ti1)
-        print(textInputLayout)
+    fun validarNombre(){
 
-        return false
-    }
+        var tietNombre = findViewById<TextInputEditText>(R.id.tiet1)
 
-    fun validarNombre(texto: String) : Boolean{
+        val texto = tietNombre.text
         var esCorrecto = true
 
-        esCorrecto = texto.isNotEmpty()
-        if (esCorrecto) esCorrecto = texto.length < 2
+        if (texto != null) {
 
-        return esCorrecto
+            if (texto.isEmpty()){
+                tietNombre.setError("TUS MUERTOS GILIPOLLAS QUE COJONES HACES")
+            }else if(texto.length < 3){
+                tietNombre.setError("El nombre no puede tener menos de 3 caracteres")
+            }else{
+                tietNombre.setError(null)
+            }
+
+        }
     }
 
-    fun validarEdad(edad: Int) : Boolean{
-        return edad < 18
+    fun validarEdad(){
+
+        var tietEdad = findViewById<TextInputEditText>(R.id.tiet2)
+        val edad = tietEdad.text
+
+        if (edad != null && edad.isNotEmpty()) {
+            if (edad.toString().toInt() < 18){
+                tietEdad.setError("Debes ser mayor de edad")
+            }else{
+                tietEdad.setError(null)
+            }
+        }
+
+
     }
 }
